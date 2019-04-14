2d2feb9f202b59185ac9d67d6bff14ed3fb184a1
Daniel Paz Avalos <me@dpazavalos.dev>
2019-04-13 19:24:58 -0500

    Update 03_Largest_Prime_Num.java
    Code is successful on test case 13195, but not on target 600851475143. Begin conversion to BigInteger

--- 03_Largest_Prime_Num.java
+++ 03_Largest_Prime_Num.java
@@ -1,8 +1,14 @@
-List<Integer> eulerList = new ArrayList<Integer>();
-int toFactor = 13195;
+/*
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
+*/
+
+public static ArrayList<Integer> eulerList = new ArrayList<Integer>();
+// public static Integer TOFACTOR = 13195;
+public static BigInteger TOFACTOR = new BigInteger("600851475143");
 
 // Tool to produce product from array
-public static Integer arrprod(ArrayList<Integer> arrInt) {
+public static Integer ArrProd(ArrayList<Integer> arrInt) {
   Integer product = 1;
   for (Integer x : arrInt){
     product *= x;
@@ -10,15 +16,24 @@ public static Integer arrprod(ArrayList<Integer> arrInt) {
   return product;
 }
 
-// Find smallest factor
-for (i; i<toFactor/2; i++; ){
-  if (toFactor%i == 0){
-    eulerList.add(i);
-    break;
+// Given The last factor found against TOFACTOR, return the next highest one
+public static Integer NextFactor(Integer lastFactor){
+  Integer next = 0;
+  for (int x=lastFactor+1; x<TOFACTOR/2; x++){
+    if (TOFACTOR%x == 0){
+      next = x;
+      break;
+    }
   }
+  return next;
 }
 
-while (arrprod(eulerList)!=toFactor) {;}
-
-
 // ((7/2) && (7%2));
+
+// Primary logic
+eulerList.add(1); // used to start factor checks. WIll not interfere with final product
+while (ArrProd(eulerList)<TOFACTOR) {
+  Integer nextFactor = NextFactor(eulerList.get(eulerList.size()-1));
+  eulerList.add(nextFactor);
+}
+
